3bd1c858bfa97c65b419b1b744e2e12a638defc8
zhayujie <yjzha1996@163.com>
2019-08-15 21:52:18 +0800

    simplify code of the On2 method

--- leetCode/03_ON.cpp
+++ leetCode/03_ON.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-
+class Solution {
+  public:
+    int lengthOfLongestSubstring(string s) {
+        int count = 0;
+        int start = 0;
+        int end = 0;
+      
+        for (int i = 1; i < s.length(); i++) {
+          for (int j = start; j < i; j++) {
+            if (s[i] == s[j]) {
+              int currentCount = end - start + 1;
+              if (currentCount > count) {
+                count = currentCount;
+              }
+              start = j + 1;
+              break;
+            }
+          }
+          end = i;
+        }
+        int lastCount = s.length() - start;
+        count = max(lastCount, count);
+        return count;
+    }
+};
 
 int main(void) {
-  string s = "";
-  int count = 1;
-  int start = 0;
-  int end = 0;
+  string s = "abcabcbb";
+  Solution soulution;
 
-  if (s.length() == 0) {
-    cout << 0 << endl; 
-    return 0;
-  }
-
-  for (int i = 1; i < s.length(); i++) {
-    for (int j = start; j < i; j++) {
-      if (s[i] == s[j]) {
-        int currentCount = end - start + 1;
-        if (currentCount > count) {
-          count = currentCount;
-        }
-        // cout << "start: " << start << " end: "<< end << " currentCount: " << currentCount << endl; 
-        start = j + 1;
-        break;
-      }
-    }
-    end = i;
-  }
-  int lastCount = s.length() - start;
-  if (lastCount > count) {
-    count = lastCount;
-  }
-
-  cout << count << endl; 
+  cout << soulution.lengthOfLongestSubstring(s) << endl; 
 
   return 0;
 }
