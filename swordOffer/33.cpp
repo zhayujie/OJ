e0e8491256363d2dde8fb193209bee4e5462d86a
zhayujie <yjzha1996@163.com>
2019-09-19 17:25:46 +0800

    增加33,34两道二叉树遍历题目

--- swordOffer/33.cpp
+++ swordOffer/33.cpp
@@ -5,10 +5,45 @@ using namespace std;
 class Solution {
 public:
     bool VerifySquenceOfBST(vector<int> sequence) {
-      
+        if (sequence.size() == 0) {
+            return false;
+        }
+        // 判断当前序列是否满足后序遍历
+        int i = 0;
+        int size = sequence.size();
+        int root = sequence[size-1];
+        while (i < size-1) {
+            if (sequence[i] > root) {
+                break;
+            }
+            i++;
+        }
+        int j = i;
+        while (j < size-1) {
+            if (sequence[j] < root) {
+                return false;
+            }
+            j++;
+        }
+        // 判断左子树是否是后序遍历
+        bool left = true, right = true;
+        if (i > 0) {
+            vector<int> leftSeq(sequence.begin(), sequence.begin()+i);
+            bool left = VerifySquenceOfBST(leftSeq);
+        }
+        // 判断右子树是否是后序遍历
+        if (j > i) {
+            vector<int> rightSeq(sequence.begin()+i, sequence.end()-1);
+            bool right = VerifySquenceOfBST(rightSeq);
+        }
+        return (left && right);
     }
 };
 
 int main(void) {
-
+    Solution solution;
+    int test[] = {5, 7, 6, 9, 11, 10, 8};
+    vector<int> vec(test, test+7);
+    bool res = solution.VerifySquenceOfBST(vec);
+    cout << res << endl;
 }
--- swordOffer/34.cpp
+++ swordOffer/34.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <algorithm>
 
 struct TreeNode {
 	int val;
@@ -11,10 +12,34 @@ struct TreeNode {
 	}
 };
 
-class Solution {
-public:
-    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+bool cmp(vector<int> v1, vector<int> v2) {
+	return v1.size() > v2.size();
+}
 
+class Solution {
+vector<vector<int> > res;
+public:
+		void FindPathCore(TreeNode* root, int expectNumber, vector<int> &path, int count) {
+			if (root == NULL) 	return;
+			int val = root->val;
+			path.push_back(val);			
+			if (root->left == NULL && root->right == NULL) {
+				if (count + val == expectNumber) {
+					res.push_back(path);			// 会调用拷贝构造函数
+				}
+			} else {
+				FindPathCore(root->left, expectNumber, path, count + val);
+				FindPathCore(root->right, expectNumber, path, count + val);
+			}
+			path.pop_back();							// 如果传vector地址，则这里需要弹出
+			return;
+    }
+
+    vector<vector<int> > FindPath(TreeNode* root,int expectNumber) {
+			vector<int> path;
+			FindPathCore(root, expectNumber, path, 0);
+			sort(res.begin(), res.end(), cmp);
+			return res;
     }
 };
 
